0754df6d1cb44bd9cabdf982c90941ffc423b7b7
wacista <wacista@student.42.fr>
2024-11-11 18:05:43 +0100

    better strjoin

--- src/exe/utils.c
+++ src/exe/utils.c
@@ -6,7 +6,7 @@
 /*   By: wacista <wacista@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 04:47:53 by wcista            #+#    #+#             */
-/*   Updated: 2024/11/07 19:26:04 by wacista          ###   ########.fr       */
+/*   Updated: 2024/11/11 17:28:10 by wacista          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,25 @@ void	ft_free_str(char **str)
 
 char	*ft_strjoin_free(char *s1, char *s2)
 {
-	char	*dest;
-	size_t	len;
 	int		i;
-	int		j;
+	char	*dest;
 
-	if ((!s1) && (!s2))
+	if (!s1 && !s2)
 		return (NULL);
-	i = ft_strlen(s1);
-	j = ft_strlen(s2);
-	len = (i + j + 1);
-	dest = (char *)malloc((len) * sizeof(char));
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!dest)
 		return (NULL);
-	i = -1;
-	j = 0;
-	while (s1[++i])
-		dest[j++] = s1[i];
-	i = -1;
-	while (s2[++i])
-		dest[j++] = s2[i];
-	dest[j] = '\0';
-	ft_free_str(&s1);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2 && *s2)
+		dest[i++] = *s2++;
+	dest[i] = '\0';
+	if (s1)
+		free(s1);
 	return (dest);
 }
 
